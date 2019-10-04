bae249098f6bf551fc24c7ddb6f7f77e5fff1b01
Vinnie Lomino <vinlomino@gmail.com>
2019-10-04 02:37:22 -0400

    Added night_light.py

---
+++ other_lights/night_light.py
@@ -0,0 +1,17 @@
+import board
+import neopixel
+import time
+import threading
+import math
+import threading
+import time
+import os
+
+fraction = 1/255
+num_of_pixels = 300
+pixels = neopixel.NeoPixel(board.D18, num_of_pixels, brightness=fraction, auto_write=False, pixel_order=neopixel.GRB)
+
+for i in range(num_of_pixels):
+    pixels[i] = (255, 255, 255)
+
+pixels.show()
