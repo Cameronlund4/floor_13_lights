4a4aae73c49f9ff62a50201ae142644f17008918
Vinnie Lomino <vinlomino@gmail.com>
2019-10-04 21:37:05 -0400

    Changed the GPIO pin to D.14

--- other_lights/turn_lights_off.py
+++ other_lights/turn_lights_off.py
@@ -9,6 +9,6 @@ import os
 
 
 num_of_pixels = 300
-pixels = neopixel.NeoPixel(board.D18, num_of_pixels, brightness=0, auto_write=False, pixel_order=neopixel.GRB)
+pixels = neopixel.NeoPixel(board.D14, num_of_pixels, brightness=0, auto_write=False, pixel_order=neopixel.GRB)
 pixels.show()
 
