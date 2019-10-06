b20f1bd8e1250f282da683e45a3e5d2f08e39784
Vinnie Lomino <vinlomino@gmail.com>
2019-10-06 02:39:57 -0400

    Added a new brightness pulse. Less intense

--- floor_13_lights.py
+++ floor_13_lights.py
@@ -172,7 +172,7 @@ def flash_lights():
                 nextBar = nextBar
                 # While we're still in this beat and don't have a new song
                 loudness = get_loudness(nextBar["start"])
-                print("Loudness:", loudness)
+                #print("Loudness:", loudness)
                 while time_until(nextBar["start"]+nextBar["duration"]) > 0 and (not resetIndex):
                     #     if (time_until(nextBar["start"]) > 0):
                     #         brightness = 0;
