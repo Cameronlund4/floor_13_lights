895b456a4df59f896c4436f0170c8c3d326fca64
Cameron Lund <cameronlund4@gmail.com>
2019-10-08 16:02:12 -0400

    Automatic gradient cycling

--- other_lights/gradient_fill.py
+++ other_lights/gradient_fill.py
@@ -23,22 +23,38 @@ def set_color(colorA, colorB):
     for i in range(50):
         perc = i/50 if i != 0 else 0
         print(perc)
-        pixels.fill(gradient(perc, colorB, colorA))
+        pixels.fill(gradient(perc, colorA, colorB))
         pixels.show()
 
 
+def wheel(pos):
+    # Input a value 0 to 255 to get a color value.
+    # The colours are a transition r - g - b - back to r.
+    if pos < 0 or pos > 255:
+        r = g = b = 0
+    elif pos < 85:
+        r = int(pos * 3)
+        g = int(255 - pos*3)
+        b = 0
+    elif pos < 170:
+        pos -= 85
+        r = int(255 - pos*3)
+        g = 0
+        b = int(pos*3)
+    else:
+        pos -= 170
+        r = 0
+        g = int(pos*3)
+        b = int(255 - pos*3)
+    # if ORDER == neopixel.RGB or ORDER == neopixel.GRB else (r, g, b, 0)
+    return (r, g, b)
+
+colors = []
+
+for i in range(0, 255):
+    colors.append(wheel(i))
+
 while True:
-    set_color([255, 127, 0], [255, 255, 0])
-    time.sleep(0.25)
-    set_color([255, 255, 0], [0, 255, 0])
-    time.sleep(0.25)
-    set_color([0, 255, 0], [0, 0, 255])
-    time.sleep(0.25)
-    set_color([0, 0, 255], [75, 0, 130])
-    time.sleep(0.25)
-    set_color([75, 0, 130], [148, 0, 211])
-    time.sleep(0.25)
-    set_color([148, 0, 211], [255, 0, 0])
-    time.sleep(0.25)
-    set_color([255, 0, 0], [255, 127, 0])
-    time.sleep(0.25)
+    for i in range(len(colors)-1):
+        set_color(colors[i], colors[i+1])
+    set_color(colors[len(colors)-1], colors[0]);
