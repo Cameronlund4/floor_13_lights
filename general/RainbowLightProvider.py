e512b71520885ce6d231bab6a9a177feb5ed8225
Cameron Lund <cameronlund4@gmail.com>
2019-10-31 13:21:28 -0400

    Another subclass fix

--- general/RainbowLightProvider.py
+++ general/RainbowLightProvider.py
@@ -3,7 +3,7 @@ from GradientFixedLightProvider import GradientFixedLightProvider
 
 class RainbowLightProvider(GradientFixedLightProvider):
     def __init__(self, colorIts=3, steps=-5):
-        super(FixedLightProvider, self).__init__(
+        super(GradientFixedLightProvider, self).__init__(
             self.gen_rainbow(steps), colorIts)
 
     def gradient(self, percent, colorA, colorB):
