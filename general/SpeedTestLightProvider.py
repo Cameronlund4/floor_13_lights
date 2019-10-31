bf13a742147a1b76ff05d091c98447c53f96b54c
Cameron Lund <cameronlund4@gmail.com>
2019-10-31 18:50:20 -0400

    Enhance speed test

--- general/SpeedTestLightProvider.py
+++ general/SpeedTestLightProvider.py
@@ -2,6 +2,7 @@ from LightProvider import LightProvider
 
 class SpeedTestLightProvider(LightProvider):
     parity = False
+    green = 0
 
     def __init__(self):
         super(SpeedTestLightProvider, self).__init__()
@@ -9,5 +10,11 @@ class SpeedTestLightProvider(LightProvider):
     # Overrides parent
     def providePixels(self, pixels):
         for i in range(len(pixels)):
-            pixels[i] = [255, 0, 0] if ((i % 2 == 0) if self.parity else (i % 2 != 0)) else [0, 0, 0]
+            if (i == self.green):
+                pixels[i] = [0, 255, 0]
+            else:
+                pixels[i] = [255, 0, 0] if ((i % 2 == 0) if self.parity else (i % 2 != 0)) else [0, 0, 0]
         self.parity = not(self.parity)
+        self.green += 1
+        if (self.green >= len(pixels)):
+            self.green = 0
