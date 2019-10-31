62cb5ed7e13af81a83d001127b4c6aa65bf1c5f5
Cameron Lund <cameronlund4@gmail.com>
2019-10-31 13:38:22 -0400

    Add rainbow debugs

--- general/RainbowLightProvider.py
+++ general/RainbowLightProvider.py
@@ -3,7 +3,9 @@ from GradientFixedLightProvider import GradientFixedLightProvider
 
 class RainbowLightProvider(GradientFixedLightProvider):
     def __init__(self, colorIts, steps):
-        super(GradientFixedLightProvider, self).__init__(self.gen_rainbow(steps), colorIts)
+        print("These be the steps")
+        print(self.gen_rainbow(steps))
+        super(GradientFixedLightProvider, self).__init__((self.gen_rainbow(steps)), colorIts)
 
     def gradient(self, percent, colorA, colorB):
         color = [0, 0, 0]
