c56e67f6e2722e0a2ef309bc357de76d0cae91b3
Cameron Lund <cameronlund4@gmail.com>
2019-10-31 20:07:57 -0400

    Gradient ball

--- general/BouncyLightProvider.py
+++ general/BouncyLightProvider.py
@@ -1,5 +1,6 @@
 from LightProvider import LightProvider
 
+
 class BouncyLightProvider(LightProvider):
     ind = 1
     direct = True
@@ -8,22 +9,25 @@ class BouncyLightProvider(LightProvider):
         super(BouncyLightProvider, self).__init__()
         self.provider = provider
 
+    def gradient(self, percent, colorA, colorB):
+        color = [0, 0, 0]
+        for i in range(3):
+            color[i] = int(colorA[i] + percent * (colorB[i] - colorA[i]))
+        return color
+
     # Overrides parent
     def providePixels(self, pixels):
         fakePixels = [None] * len(pixels)
         self.provider.providePixels(fakePixels)
         for i in range(len(pixels)):
-            if abs(i - self.ind) <= 2:
-                if abs(i - self.ind) <= 1:
-                    pixels[i] = [0, 0, 0]
-                else:
-                    pixels[i] = [0, 255, 0]
+            if abs(i - self.ind) <= 4:
+                pixels[i] = self.gradient(abs(i - self.ind)/4, fakePixels[i], [0, 255, 0])
             else:
                 pixels[i] = fakePixels[i]
-        
-        if (self.ind >= (len(fakePixels)-2)):
+
+        if (self.ind >= (len(fakePixels)-4)):
             self.direct = False
-        elif (self.ind <= 2):
+        elif (self.ind <= 4):
             self.direct = True
 
         if (self.direct):
