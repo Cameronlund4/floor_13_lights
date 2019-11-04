76a3343916a915c72cb4de7d7c10173757311ad3
Cameron Lund <cameronlund4@gmail.com>
2019-11-04 12:17:14 -0500

    Bring the OG ball back

--- general/BouncyLightProvider.py
+++ general/BouncyLightProvider.py
@@ -4,33 +4,42 @@ from LightProvider import LightProvider
 class BouncyLightProvider(LightProvider):
     ind = 1
     direct = True
+    ball_width = 3  # Needs to be odd
+    ball_fill = [0, 0, 0]
+    ball_border = [255, 255, 255]
 
-    def __init__(self, provider):
+    def __init__(self, provider, ball_width=1, ball_fill=[0,0,0], ball_border=[0, 255, 0]):
         super(BouncyLightProvider, self).__init__()
         self.provider = provider
-
-    def gradient(self, percent, colorA, colorB):
-        color = [0, 0, 0]
-        for i in range(3):
-            color[i] = int(colorA[i] + percent * (colorB[i] - colorA[i]))
-        return color
+        self.ball_width = (ball_width * 2) + 1
+        self.ball_fill = ball_fill
+        self.ball_border = ball_border
 
     # Overrides parent
     def providePixels(self, pixels):
+        halfBall = (self.ball_width-1)/2
+
+        # Get fake pixels
         fakePixels = [None] * len(pixels)
         self.provider.providePixels(fakePixels)
+
+        # Render the ball
         for i in range(len(pixels)):
-            if abs(i - self.ind) <= 30:
-                pixels[i] = self.gradient(
-                    1-((abs(i - self.ind)/30)*.75), fakePixels[i], [0, 0, 0])
+            if abs(i - self.ind) <= halfBall:
+                if (abs(i-self.ind) == halfBall):
+                    pixels[i] = self.ball_border
+                else:
+                    pixels[i] = self.ball_fill
             else:
                 pixels[i] = fakePixels[i]
 
-        if (self.ind >= (len(fakePixels)-30)):
+        # Handle switching the direction of the ball if necessary
+        if (self.ind >= (len(fakePixels)-halfBall)):
             self.direct = False
-        elif (self.ind <= 30):
+        elif (self.ind <= halfBall):
             self.direct = True
 
+        # Move the ball
         if (self.direct):
             self.ind += 1
         else:
