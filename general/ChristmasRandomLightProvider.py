63e23112d08589c25d0370f39147a778d3ef68c6
Cameron Lund <cameronlund4@gmail.com>
2019-12-05 13:20:52 -0500

    Basic christmas tree test

--- general/ChristmasRandomLightProvider.py
+++ general/ChristmasRandomLightProvider.py
@@ -2,8 +2,9 @@ from FixedLightProvider import FixedLightProvider
 import random
 
 
-class ChristmasRandomLightProvider(FixedLightProvider):
+class ChristmasRandomLightProvider(LightProvider):
     steps = []
+    treeColor = [31, 77, 9]
     christmasColors = [
         [234, 13, 13],
         [251, 111, 36],
@@ -12,16 +13,57 @@ class ChristmasRandomLightProvider(FixedLightProvider):
         [137, 52, 184]
     ]
 
-    def __init__(self, light_width=6, picks=50):
-        super(ChristmasRandomLightProvider, self).__init__(
-            self.gen_random(self.christmasColors, picks, light_width))
+    example_light = ["color", "timeAlive"]
+    liveLights = None
+    MIN_DISTANCE = 6
+    WIDTH = 3 # Should be odd
+    START_EDGE_BRIGHTNESS = .75
+    END_EDGE_BRIGHTNESS = .25
+    MAX_LIGHTS = 10
+    RENDER_PERCENTAGE = .1
 
-    def gen_random(self, colors, picks, light_width):
-        output = []
-        for i in range(picks):
-            choice = random.choice(colors)
-            for j in range(light_width):
-                output.append([31, 77, 9])
-            for j in range(2):
-                output.append(choice)
-        return output
+    def __init__(self, light_width=6, picks=50):
+        super(ChristmasRandomLightProvider, self).__init__()
+
+    # choice = random.choice(colors)
+    def pick_light_pos(MIN_DISTANCE, liveLights, good_indexes=[]):
+        if not good_indexes:
+            # TODO Find good indexes
+            pass
+        return 0 # TODO Implement
+
+    def gradient(self, percent, colorA, colorB):
+            color = [0, 0, 0]
+            for i in range(3):
+                color[i] = int(colorA[i] + percent * (colorB[i] - colorA[i]))
+            return color
+
+    # Set next frame of pixels
+    def providePixels(self, pixels):
+        # If we don't have our array, make them with the length of our lights
+        if not self.liveLights:
+            self.liveLights = [None] * len(pixels)
+            self.liveLights[149] = [234, 13, 13]
+
+        # Fill the cache with green (the color of the christmas light)
+        cache = [self.treeColor] * len(pixels)
+
+        # TODO Draw tree and lights
+        for i in range(self.liveLights):
+            if not self.liveLights[i]:
+                continue
+
+            light = self.liveLights[i]
+
+            cache[i] = light[0]
+
+            # TODO Finish
+            # # Draw the light centered at i
+            # for j in range(i-((self.WIDTH-1)/2), 1+i+((self.WIDTH-1)/2)):
+            #     # Make the center full brightness, make the edges less bright
+
+            #     percentage = abs(i-j)
+
+        # Draw the cache into the pixels
+        for i in range(cache):
+            pixels[i] = cache[i]
